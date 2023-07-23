668a6bc48cce923fbf0f0af609e10e91fef63ee7
Jason Lee Ken Sang <jason.lee@elemy.com>
2023-07-24 00:17:22 +0100

    basic load and predict function implementation for truss

--- model/model.py
+++ model/model.py
@@ -1,66 +1,37 @@
-import torch
-from fairscale.nn.model_parallel.initialize import (
-    get_model_parallel_rank,
-    initialize_model_parallel,
-    model_parallel_is_initialized,
-)
+from typing import Dict
 
-from model.model import ModelArgs, Transformer
-from model.tokenizer import Tokenizer
-
+from model.generation import Llama
 
 class Model:
     def __init__(self, **kwargs) -> None:
         self._data_dir = kwargs["data_dir"]
         self._config = kwargs["config"]
         self._secrets = kwargs["secrets"]
-        self._tokenizer = None
-        self._model = None
+        self._generator = None
 
     def load(self):
         ckpt_dir = "../data/llama-2-7b/"
         tokenizer_path = "../data/tokenizer.model"
-        if not torch.distributed.is_initialized():
-            torch.distributed.init_process_group("nccl")
-        if not model_parallel_is_initialized():
-            if model_parallel_size is None:
-                model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
-            initialize_model_parallel(model_parallel_size)
-
-        local_rank = int(os.environ.get("LOCAL_RANK", 0))
-        torch.cuda.set_device(local_rank)
-
-        # seed must be the same in all processes
-        torch.manual_seed(1)
-
-        if local_rank > 0:
-            sys.stdout = open(os.devnull, "w")
-
-        start_time = time.time()
-        checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
-        assert len(checkpoints) > 0, f"no checkpoint files found in {ckpt_dir}"
-        assert model_parallel_size == len(
-            checkpoints
-        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
-        ckpt_path = checkpoints[get_model_parallel_rank()]
-        checkpoint = torch.load(ckpt_path, map_location="cpu")
-        with open(Path(ckpt_dir) / "params.json", "r") as f:
-            params = json.loads(f.read())
-
-        model_args: ModelArgs = ModelArgs(
-            max_seq_len=max_seq_len,
-            max_batch_size=max_batch_size,
-            **params,
+        
+        self._generator = Llama.build(
+            ckpt_dir=ckpt_dir,
+            tokenizer_path=tokenizer_path,
+            max_seq_len=512,
+            max_batch_size=4,
         )
-        tokenizer = Tokenizer(model_path=tokenizer_path)
-        model_args.vocab_size = tokenizer.n_words
-        torch.set_default_tensor_type(torch.cuda.HalfTensor)
-        model = Transformer(model_args)
-        model.load_state_dict(checkpoint, strict=False)
-        print(f"Loaded in {time.time() - start_time:.2f} seconds")
 
-    def predict(self, model_input: Any) -> Any:
-        model_output = {}
-        # Invoke model on model_input and calculate predictions here.
-        model_output["predictions"] = []
-        return model_output
+    def predict(self, request: Dict) -> Dict:
+        with torch.no_grad():
+            try:
+                dialogs = [
+                    {"role": "user", "content": f"{request.pop('prompt')}"}
+                ]
+                results = self._generator.chat_completion(
+                    dialogs,
+                    max_gen_len=None,
+                    temperature=0.6,
+                    top_p=0.9,
+                )
+                return {"data": f"{result['generation']['content']}"}
+            except Exception as exc:
+                return {"status": "error", "data": None, "message": str(exc)}
